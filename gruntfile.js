daed95bf362f03285b84eba4815b55d015aa5bbf
Stella Ma <sm873x@gmail.com>
2016-08-01 13:33:57 -0400

    Removed vendor jquery js files from grunt concat

--- gruntfile.js
+++ gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
                 options: {
                     sourceMap: true
                 },
-                src: ['src/js/shop.module.js', 'src/js/**/*.js'],
+                src: ['src/js/shop.module.js', 'src/js/*.js'],
                 dest: 'build/js/main.js'
             }
         },
