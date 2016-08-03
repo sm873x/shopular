b1a443797d29af83f4e8ac4c1e60899fd1be4be4
Stella Ma <sm873x@gmail.com>
2016-08-03 14:31:06 -0400

    Cleared input fields with clearing of newItem object in controller after newItem is successfully added

--- src/js/inventory.controller.js
+++ src/js/inventory.controller.js
@@ -15,23 +15,20 @@
     function InventoryController(theLocalStorService) {
         var that = this;
 
-        this.orderByField = 'price';
-        this.reverseSort = false;
-
         this.tax = tax;
         this.getPrice = getPrice;
+
+        this.orderByField = 'price';
+        this.reverseSort = false;
 
         this.inventory = theLocalStorService.getAll();
 
         this.newItem = {};
 
         this.addItem = function addItem (item) {
-            console.log(item);
             that.newItem = theLocalStorService.saveItem(item);
-            console.log(that.newItem);
+            that.newItem = {};
         };
-
-        // that.newItem = {};
 
     }
 
